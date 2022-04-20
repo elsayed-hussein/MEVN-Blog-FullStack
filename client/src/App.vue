61dbfbce86e13aaec33c50f0fc720b14ae10aa20
elsayed hussein <elsayed.hussen@gmail.com>
2022-04-20 02:50:28 +0200

    36 get post by id done

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,24 +3,34 @@
     <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title>
-           Elsayed Hussein Blog 
-           </v-list-item-title>
-          <v-list-item-subtitle>
-           MEVN full stak App 
-           </v-list-item-subtitle>
+          <v-list-item-title> Elsayed Hussein Blog </v-list-item-title>
+          <v-list-item-subtitle> MEVN full stack App </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
+      <v-divider> </v-divider>
+      <v-list dense>
+        <v-list-item-group color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.link" link>
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
       <!--  -->
     </v-navigation-drawer>
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Elsayed Hussein Blog</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
+      <router-view></router-view>
       <!--  -->
     </v-main>
   </v-app>
@@ -28,6 +38,13 @@
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Home", icon: "mdi-home", link: "/",  },
+      { title: "Add Post", icon: "mdi-note-plus", link: "/add-post",  },
+      { title: "About", icon: "mdi-help-box", link: "/about", },
+      ],
+  }),
 };
 </script>
---
+++ client/src/views/AddPostView.vue
@@ -0,0 +1,11 @@
+<template>
+<h1>Add Post Page</h1>
+</template>
+<script>
+
+  export default {
+    name: 'AddPost',
+
+ 
+  }
+</script>
