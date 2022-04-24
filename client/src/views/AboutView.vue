5e34c49e8b4a776d33a2ef8606c424cc9e24123b
elsayed hussein <elsayed.hussen@gmail.com>
2022-04-24 14:03:20 +0200

    done for deployment

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>About Page</h1>
+  <v-container>
+    <v-row no-gutters>
+      <v-col sm="10" class="mx-auto pa-5">
+       <h1>MEVN APP</h1>
+       <h2>Made By Elsayed Hussein </h2>
+       <p>
+         <ul>
+           <li><a href="https://www.linkedin.com/in/elsayed-hussein-68188a164/">linkedin</a></li>
+           <li><a href="https://github.com/elsayed-hussein">github</a></li>
+         </ul>
+       </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
